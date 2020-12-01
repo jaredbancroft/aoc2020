dd1285a5e87ba94bb56cce1a997661e8beb991a5
Jared Bancroft <jaredb@broadinstitute.org>
2020-12-01 15:26:14 -0500

    updated help text for day 1

--- cmd/day1.go
+++ cmd/day1.go
@@ -15,12 +15,19 @@ var day1Cmd = &cobra.Command{
 	Long: `
 	Advent of Code 2020 - Day 1
 	
+	--- Part One ---
 	Before you leave, the Elves in accounting just need you to fix 
 	your expense report (your puzzle input); apparently, something 
 	isn't quite adding up.
 
 	Specifically, they need you to find the two entries that 
-	sum to 2020 and then multiply those two numbers together.`,
+	sum to 2020 and then multiply those two numbers together.
+	
+	--- Part Two ---
+	The Elves in accounting are thankful for your help; one of them 
+	even offers you a starfish coin they had left over from a past vacation. 
+	They offer you a second one if you can find three numbers in your expense 
+	report that meet the same criteria.`,
 	RunE: func(cmd *cobra.Command, args []string) error {
 		expenses, err := helpers.ReadIntFile(input)
 		if err != nil {

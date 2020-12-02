b53cea8b9cefcab257131efd2c9fbcb6ae7d8c1d
Jared Bancroft <jaredb@broadinstitute.org>
2020-12-01 22:01:21 -0500

    add go report card

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
+[![Go Report Card](https://goreportcard.com/badge/github.com/jaredbancroft/aoc2020)](https://goreportcard.com/report/github.com/jaredbancroft/aoc2020)
+
 # Advent of Code 2020
 My attempt at [Advent of Code 2020](https://adventofcode.com) using [Golang](https://golang.org/)  
--- cmd/root.go
+++ cmd/root.go
@@ -12,7 +12,22 @@ var input string
 var rootCmd = &cobra.Command{
 	Use:   "aoc2020",
 	Short: "Advent of Code 2020",
-	Long:  `Saving Christmas with code`,
+	Long: `
+	Advent of Code 2020
+
+	After saving Christmas five years in a row, you've decided to take a vacation at a nice 
+	resort on a tropical island. Surely, Christmas will go on without you.
+
+	The tropical island has its own currency and is entirely cash-only. 
+	The gold coins used there have a little picture of a starfish; the locals just call them stars. 
+	None of the currency exchanges seem to have heard of them, but somehow, you'll need to find 
+	fifty of these coins by the time you arrive so you can pay the deposit on your room.
+	
+	To save your vacation, you need to get all fifty stars by December 25th.
+	
+	Collect stars by solving puzzles. Two puzzles will be made available on each day 
+	in the Advent calendar; the second puzzle is unlocked when you complete the first. 
+	Each puzzle grants one star. Good luck!`,
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
